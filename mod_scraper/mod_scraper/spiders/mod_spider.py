ff1f8dfbf2bfc447aa8d53f752908d2171746d8b
hide-ous <mattia.samory@gmail.com>
2021-06-21 17:56:06 +0200

    * parse conforms to old.reddit about/moderators

--- mod_scraper/mod_scraper/spiders/mod_spider.py
+++ mod_scraper/mod_scraper/spiders/mod_spider.py
@@ -1,25 +1,25 @@
-from datetime import datetime as dt
 import scrapy
+
 
 class RedditSpider(scrapy.Spider):
     name = 'reddit'
 
     def start_requests(self):
-        yield scrapy.Request('http://reddit.com')
+        yield scrapy.Request('https://www.reddit.com/r/conspiracy/about/moderators')
 
     def parse(self, response):
         items = []
-        for div in response.css('div.sitetable div.thing'):
-            try:
-                title = div.css('p.title a::text').extract_first()
-                votes_div = div.css('div.score.unvoted')
-                votes = votes_div.css('::attr(title)').extract_first()
-                votes = votes or votes_div.css('::text').extract_first()
+        for div in response.css('div.moderator-table'):
+            for row in div.css('tr'):
+                cur_user = dict()
+                try:
+                    cur_user['name'] = row.css('span.user a::text').extract_first()
+                    cur_user['karma'] = row.css('span.user b::text').extract_first()
+                    cur_user['since'] = row.css('time::attr(datetime)').extract_first()
+                    cur_user['permissions'] = row.css('span.permission-bit::text').extract_first()
 
-                items.append({'title': title, 'votes': int(votes)})
-            except:
-                pass
-
-        if len(items) > 0:
-            timestamp = response.meta['wayback_machine_time'].timestamp()
-            return {'timestamp': timestamp, 'items': items}
+                    items.append(cur_user)
+                except:
+                    pass
+        timestamp = response.meta['wayback_machine_time'].timestamp()
+        return {'timestamp': timestamp, 'items': items}
